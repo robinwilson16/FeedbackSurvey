2dd75d76461b178ca6ff3843a782420cf14fdb38
Robin Wilson <robin.wilson@robindigital.co.uk>
2025-08-14 00:34:28 +0100

    Completed first version

--- FeedbackSurvey/wwwroot/js/speachInterop.js
+++ FeedbackSurvey/wwwroot/js/speachInterop.js
@@ -24,5 +24,57 @@
             synth.speak(speach);
         }, "1000");
         console.log("Welcome message spoken.");
+    },
+    sayThankYouMessage: function () {
+        const synth = window.speechSynthesis;
+
+        let voices = [];
+        voices = synth.getVoices();
+
+        setTimeout(() => {
+
+            voices = synth.getVoices();
+            //alert(voices);
+
+            const selectedVoice = "Microsoft Libby Online (Natural) - English (United Kingdom)";
+
+            let speach = new SpeechSynthesisUtterance("Thank you for using the feedback station! Scan the QR code with your device to tell us more.");
+
+            for (const voice of voices) {
+                //alert(voice.name);
+                if (voice.name === selectedVoice) {
+                    speach.voice = voice;
+                }
+            }
+
+            synth.speak(speach);
+        }, "1000");
+        console.log("Thank you message spoken.");
+    },
+    sayErrorMessage: function () {
+        const synth = window.speechSynthesis;
+
+        let voices = [];
+        voices = synth.getVoices();
+
+        setTimeout(() => {
+
+            voices = synth.getVoices();
+            //alert(voices);
+
+            const selectedVoice = "Microsoft Libby Online (Natural) - English (United Kingdom)";
+
+            let speach = new SpeechSynthesisUtterance("Please wait for assistance.");
+
+            for (const voice of voices) {
+                //alert(voice.name);
+                if (voice.name === selectedVoice) {
+                    speach.voice = voice;
+                }
+            }
+
+            synth.speak(speach);
+        }, "1000");
+        console.log("Error message spoken.");
     }
 };
